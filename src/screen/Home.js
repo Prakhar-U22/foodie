45629f20647198add1ddad024b67d63f2cf7f1f1
Prakhar Ujjaliya <prakharujjaliya03@gmail.com>
2024-06-08 18:43:30 +0530

    first commit

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -3,9 +3,8 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Card from '../components/Card';
 import burgerImg from '../Img/burger.jpg'; // Import the image
-import pizzaImage from '../Img/pizza.jpg';
-import sandwichImg from '../Img/sandwick.jpeg'; // Import the image
-
+import pizzaImg from '../Img/pizza.jpg'; // Corrected image import
+import sandwichImg from '../Img/sandwich.jpeg'; // Corrected image import
 
 export default function Home() {
   const [search, setSearch] = useState('');
@@ -21,10 +20,8 @@ export default function Home() {
         },
       });
       response = await response.json();
-
-        setFoodItems(response[0]);
-        setFoodCat(response[1]);
-
+      setFoodItems(response[0]);
+      setFoodCat(response[1]);
     } catch (error) {
       console.error('Failed to fetch data', error);
     }
@@ -37,61 +34,59 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-              <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-caption" style={{ zIndex: '10' }}>
-                      <div className="d-flex justify-content-center" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-                      </div>
-                    </div>
-                  <div className="carousel-item active">
-                    <img
-                      src={burgerImg}
-                      style={{ width: '90vh', height: '90vh', filter: 'brightness(80%)' }}
-                      className="d-block w-100"
-                      alt="Burger 1"
-                    />
-                  </div>
-                  <div className="carousel-item">
-                    <img
-                      src={pizzaImage}
-                      style={{ width: '90vh', height: '90vh', filter: 'brightness(80%)' }}
-                      className="d-block w-100"
-                      alt="Pizza"
-                    />
-                  </div>
-                  <div className="carousel-item">
-                    <img
-                      src={sandwichImg}
-                      style={{ width: '90vh', height: '90vh', filter: 'brightness(80%)' }}
-                      className="d-block w-100"
-                      alt="Sandwich"
-                    />
-                  </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
-              </div>
+      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-caption" style={{ zIndex: '10' }}>
+            <div className="d-flex justify-content-center" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
+          </div>
+          <div className="carousel-item active">
+            <img
+              src={burgerImg}
+              style={{ width: '90vh', height: '90vh', filter: 'brightness(80%)' }}
+              className="d-block w-100"
+              alt="Burger 1"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src={pizzaImg}
+              style={{ width: '90vh', height: '90vh', filter: 'brightness(80%)' }}
+              className="d-block w-100"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src={sandwichImg}
+              style={{ width: '90vh', height: '90vh', filter: 'brightness(80%)' }}
+              className="d-block w-100"
+              alt="Sandwich"
+            />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
 
       <div className="container">
         {foodCat.length > 0 ? (
           foodCat.map((data) => (
-            <div  className='row'>
-              <div key={data._id} className="fs-3 m-3">{data.CategoryName}</div>
+            <div className='row' key={data._id}>
+              <div className="fs-3 m-3">{data.CategoryName}</div>
               <hr />
               {foodItems
-                .filter((item) => (item.CategoryName === data.CategoryName)&&(item.name.toLowerCase().includes(search.toLowerCase())))
+                .filter((item) => item.CategoryName === data.CategoryName && item.name.toLowerCase().includes(search.toLowerCase()))
                 .map((filteredItem) => (
                   <div key={filteredItem._id} className="col-12 col-md-6 col-lg-3">
-                    {/* props bhejna h card mein*/}
-                    <Card foodName={filteredItem.name} foodImg={filteredItem.img} options={filteredItem.options[0]}
-                    />
+                    <Card foodName={filteredItem.name} foodImg={filteredItem.img} options={filteredItem.options[0]} />
                   </div>
                 ))}
             </div>
